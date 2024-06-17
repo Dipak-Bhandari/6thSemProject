3cfbb835391fe25f3d1935678a35c0e2b8072890
Dipak-Bhandari <nescoz.dipak@gmail.com>
2024-06-17 18:07:12 +0545

    main file modified app successfully running

--- app/src/main/java/com/aquatrace/firstapp/MainActivity.java
+++ app/src/main/java/com/aquatrace/firstapp/MainActivity.java
@@ -1,6 +1,8 @@
 package com.aquatrace.firstapp;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,16 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                startActivity(new Intent
+                        (MainActivity.this, MainActivity2.class));
+                finish();
+
+            }
+        },3000);
+
     }
 }
